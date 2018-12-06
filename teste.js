f86915be57bc22b675b143f5ac3efa276dcf0fde
Guilherme Afonso <guilherme.asp@live.com>
2018-12-05 21:11:55 -0300

    Criptografia pronta

--- teste.js
+++ teste.js
@@ -6,6 +6,36 @@ if(frase.length % 2 != 0){
 }
 pares = frase.replace(/\s/g, "").match(/.{1,2}/g)
 console.log(pares)
+
+let numbers = {
+    1 : 'a',
+    2 : 'b',
+    3 : 'c',
+    4 : 'd',
+    5 : 'e',
+    6 : 'f',
+    7 : 'g',
+    8 : 'h',
+    9 : 'i',
+    10 : 'j',
+    11 : 'k',
+    12 : 'l',
+    13 : 'm',
+    14 : 'n',
+    15 : 'o',
+    16 : 'p',
+    17 : 'q',
+    18 : 'r',
+    19 : 's',
+    20 : 't',
+    21 : 'u',
+    22 : 'v',
+    23 : 'w',
+    24 : 'x',
+    25 : 'y',
+    0 : 'z'
+}
+
 let result = {
     'a': 1,
     'b': 2,
@@ -45,10 +75,17 @@ for(let i = 0; i < pares.length; i++){
 }
 
 pares2 = []
+aux = []
 for(let i = 0, j = 0; i < paresOrdenados.length; i++){
-    soma = 0
-    for(let j = 0; j < paresOrdenados[i].length; j++){
-        soma += senha[i][i] * paresOrdenados[j][0]
-    }
-    console.log(soma)
+    aux.push(paresOrdenados[i][0]*senha[0][0] + paresOrdenados[i][1]*senha[0][1])
+    aux.push(paresOrdenados[i][0]*senha[1][0] + paresOrdenados[i][1]*senha[1][1])
+    pares2.push(aux)
+    aux = []
 }
+pronto = ""
+for(let i = 0; i < pares2.length; i++){
+    for(let j = 0; j < pares2[i].length; j++){
+        pronto += numbers[pares2[i][j]%26]
+    }
+}
+console.log(pronto)
