c4365545412e90106107cc47538fb64b3fe18a65
Guilherme Afonso <guilherme.asp@live.com>
2018-12-12 23:49:39 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const teste = require('teste.js')
 
-senha = [[1, 3], [0, 3]]
+senha = [[1, 2], [1, 5]]
 decode2 = "avlc"
-frase = "Testando frase"
+frase = "eu amo avlc"
 
 
 frase1 = teste.encode(frase, senha)
-console.log(frase1)
-frase2 = teste.decode(frase1, senha)
-console.log(frase2)
-
-aaaaa = teste.cifraCesar('guilherme', 1)
-console.log(aaaaa)
-aaaaa = teste.cifraCesar('hvjmifsnf', -1)
-console.log(aaaaa)
+console.log("Frase cifrada: " + frase1.fraseFinal)
+frase2 = teste.decode(frase1.fraseFinal, senha, frase1.espacos)
+console.log("Frase decifrada: " + frase2)  
